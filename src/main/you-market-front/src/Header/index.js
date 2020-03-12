6733513891671af42c7056f20d51275d0057d512
manuelagt <manuelagt@icloud.com>
2020-03-12 22:22:42 +0100

    S1 - Header
    Creación de la cabecera que contendrá la navegación de la página

---
+++ src/main/you-market-front/src/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './styles.css';
+import logo from '../logo.png';
+import cart from './shopping-cart-solid.svg';
+
+function Header() {
+  return(
+  <div className="container">
+   <img className="logo" src={logo} alt="Logo"/>
+   <div className="links">
+   	<a className="link" href="../App.js">Productos</a>
+   	<a className="link" href="">Dietas</a>
+   	<a className="link" href="">Cestas</a>
+   	<a className="link" href="">Carro</a>
+   </div>
+  </div>	
+ );
+}
+
+export default Header;
