ed2089c4ab29f3e63a7f9f859701250593406260
manuelagt <manuelagt@icloud.com>
2020-03-13 14:56:02 +0100

    S1 - Header - Styles
    Updated classnames to correct ones

--- src/main/you-market-front/src/Header/index.js
+++ src/main/you-market-front/src/Header/index.js
@@ -5,13 +5,13 @@ import cart from './shopping-cart-solid.svg';
 
 function Header() {
   return(
-  <div className="container">
+  <div className="header-container">
    <img className="logo" src={logo} alt="Logo"/>
-   <div className="links">
-   	<a className="link" href="../App.js">Productos</a>
-   	<a className="link" href="">Dietas</a>
-   	<a className="link" href="">Cestas</a>
-   	<a className="link" href="">Carro</a>
+   <div className="header-links">
+   	<a className="header-link" href="../App.js">Productos</a>
+   	<a className="header-link" href="">Dietas</a>
+   	<a className="header-link" href="">Cestas</a>
+   	<a className="header-link" href="">Carro</a>
    </div>
   </div>	
  );
