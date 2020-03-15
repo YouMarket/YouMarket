9b62be877ed546e23df1ac3e1fa082c2445dfa64
José Manuel Muñiz <josemamup@gmail.com>
2020-03-16 00:41:27 +0100

    Añadida navegación de dietas

--- src/main/you-market-front/src/Dietas/index.js
+++ src/main/you-market-front/src/Dietas/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './styles.css';
 import Dieta from '../Dieta';
+import Header from '../Header';
 
 function Dietas() {
   return(
-  <div className="productos-container">
-  	<div className="grid">
-	  	<Dieta tipo="Vegetariana"/>
-	  	<Dieta tipo="Mediterránea"/>
-	  	<Dieta tipo="Hiperproteica"/>
-  	</div>
+  <div>
+  	  <Header/>
+	  <div className="dietas-container">
+	  	<div className="grid">
+		  	<Dieta tipo="Vegetariana" imagen="https://i.blogs.es/120994/brocoli/450_1000.jpg"/>
+		  	<Dieta tipo="Mediterránea" imagen="https://cdn.aarp.net/content/dam/aarp/health/healthy-living/2016/09/1140-medit-diet-esp.imgcache.revd53e4a281b35d80e0dbbdbab012c6186.jpg"/>
+		  	<Dieta tipo="Hiperproteica" imagen="https://www.prensalibre.com/wp-content/uploads/2019/08/Prote%C3%ADnas-2.jpg?quality=82&w=760&h=430&crop=1"/>
+	  	</div>
+	  </div>
   </div>
  );
 }
