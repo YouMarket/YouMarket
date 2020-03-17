c462faff35c4a11c5e68ced85304e57854bf5286
Lucía del Carmen Fuentes <luciadelcarmenfuentes@gmail.com>
2020-03-17 19:11:40 +0100

    S1 - Carro - Frontend
    Añadido el precio final

--- src/main/you-market-front/src/Carro/index.js
+++ src/main/you-market-front/src/Carro/index.js
@@ -16,6 +16,7 @@ function Carro() {
 				<ProductoListado id="1" nombre="leche" supermercado="Mercadona" precio="1,3" imagen="https://cutt.ly/leche" unidad="€/litro" cantidad="1"/>
 				<ProductoListado id="1" nombre="leche" supermercado="Mercadona" precio="1,3" imagen="https://cutt.ly/leche" unidad="€/litro" cantidad="1"/>
 
+				<div className="price"><b>Precio final:</b> 15€</div>
 				<button className="button-finish">Terminar</button>
 			</div>
 
