b914bbc64d43134a292ffefeea59f8e83d4386ad
AlvaroEsteban <alvaroestebancastrillo@gmail.com>
2020-03-23 11:45:36 +0100

    Ampliado formulario

--- src/main/you-market-front/src/Registro/index.js
+++ src/main/you-market-front/src/Registro/index.js
@@ -4,6 +4,7 @@ import Header from '../Header';
 import Receta from '../Receta';
 import {InputText} from 'primereact/inputtext';
 import  {withFormik, Form, Field, ErrorMessage}  from 'formik';
+import {Card} from 'primereact/card';
 
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -27,41 +28,79 @@ function RegistroUsuario(props) {
 	<div>	  
 	  <Header/>
 	  <div className="registro-container">
-	  	<div className="grid">
-		  	<Form>
-				<div className="row">
-				
-			
+	  
 
-				</div>
-				<div className="row">
-					Email:
-					<Field name="email" type="email" className="input" />
-					<ErrorMessage name="email">
-						{message => <div className="error">{message}</div>}
-					</ErrorMessage>
-				</div>
+			<Card title="Datos del usuario">
+				<Form>
+					<div className="row">
+						Nombre: 
+						<Field name="nombre" type="text" className="input" />
+						<ErrorMessage name="nombre">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+					<div className="row">
+						Apellidos: 
+						<Field name="apellidos" type="text" className="input" />
+						<ErrorMessage name="apellidos">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+					<div className="row">
+						DNI: 
+						<Field name="dni" type="text" className="input" />
+						<ErrorMessage name="dni">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+					<div className="row">
+						Fecha de nacimiento: 
+						<Field name="fechaNacimiento" type="text" className="input" />
+						<ErrorMessage name="fechaNacimiento">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+					<div className="row">
+						Teléfono:
+						<Field name="telefono" type="text" className="input" />
+						<ErrorMessage name="telefono">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
 
-				<div className="row">
-					Password:
-					<Field name="password" type="password" className="input" />
-					<ErrorMessage name="password">
-						{message => <div className="error">{message}</div>}
-					</ErrorMessage>
-				</div>
 
-				<div className="row">
-					<button
-						type="submit"
-						className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
-						disabled={isSubmitting || !isValid}
-					>
-						Submit
-					</button>
-				</div>
-        	</Form>
+					<div className="row">
+						Email:
+						<Field name="email" type="email" className="input" />
+						<ErrorMessage name="email">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+					<div className="row">
+						Password:
+						<Field name="password" type="password" className="input" />
+						<ErrorMessage name="password">
+							{message => <div className="error">{message}</div>}
+						</ErrorMessage>
+					</div>
+
+					<div className="row">
+						<button
+							type="submit"
+							className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
+							disabled={isSubmitting || !isValid} >
+							Submit
+						</button>
+					</div>
+				</Form>
+			</Card>
+
+			<Card title="Método de pago" className="tarjeta">
+
+
+			</Card>
+		  	
 	  	</div>
-	  </div>
 	</div>
  );
 }
