28fc154f30127c9e7eeaa8b1c264011da1dbcd67
José Manuel Muñiz <josemamup@gmail.com>
2020-03-24 20:28:22 +0100

    S2 - Show y create listos

--- src/main/you-market-front/src/App.js
+++ src/main/you-market-front/src/App.js
@@ -6,10 +6,10 @@ import {
 	} from "react-router-dom";
 import Productos from './Productos';
 import Dietas from './Dietas';
-
 import RecetasListado from './RecetasListado';
 import ProductoDetalle from './ProductoDetalle';
-
+import DietaDetalle from './DietaDetalle';
+import DietaForm from './DietaForm';
 
 import Carro from './Carro';
 	
@@ -29,6 +29,15 @@ function App() {
 		    	<Route path="/dieta/list">
     				<Dietas />
     			</Route>
+    			
+    			<Route path="/show/dieta/:id">
+					<DietaDetalle />
+				</Route>
+				
+    			<Route path="create/dieta">
+					<DietaForm />
+				</Route>
+			
     			<Route path="/recetas">
     				<RecetasListado/>
     			</Route>
