17db17ed48557de308f81701349ae094ffa8e273
AlvaroEsteban <alvaroestebancastrillo@gmail.com>
2020-03-26 10:29:48 +0100

    Formulario de registro

--- src/main/you-market-front/src/Registro/index.js
+++ src/main/you-market-front/src/Registro/index.js
@@ -1,7 +1,6 @@
 import React, { Component, useState } from 'react';
 import './styles.css';
 import Header from '../Header';
-import Receta from '../Receta';
 import {InputText} from 'primereact/inputtext';
 import  {withFormik, Form, Field, ErrorMessage}  from 'formik';
 import {Card} from 'primereact/card';
@@ -9,120 +8,137 @@ import {Card} from 'primereact/card';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import { render } from 'react-dom';
 
 
+export default class RegistroUsuario extends React.Component{
 
-function RegistroUsuario(props) {
+	constructor (props){
+
+		super(props);
+		this.state = {
+			nombre: '',
+			apellidos: '',
+			dni: '', 
+			fechaNacimiento: '', 
+			telefono: '',
+			email: '',
+			password: ''
+		}
+
+	}
+
+	handleSubmit = (e) => {
 	
+		
+
+	}
+
+	handleChange = (e) => {
+		this.setState({ inputTerms: e.target.checked })
+		console.log(e.target.checked)
+	}
 	
 
-	const {
-		isSubmitting, 
-		isValid,
-	} = props;
-
-	const [nombre, setNombre] = useState(0);
-
-
-  	return(
-	<div>	  
-	  <Header/>
-	  <div className="registro-container">
-	  
-
-			<Card title="Datos del usuario">
-				<Form>
-					<div className="row">
-						Nombre: 
-						<Field name="nombre" type="text" className="input" />
-						<ErrorMessage name="nombre">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-					<div className="row">
-						Apellidos: 
-						<Field name="apellidos" type="text" className="input" />
-						<ErrorMessage name="apellidos">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-					<div className="row">
-						DNI: 
-						<Field name="dni" type="text" className="input" />
-						<ErrorMessage name="dni">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-					<div className="row">
-						Fecha de nacimiento: 
-						<Field name="fechaNacimiento" type="text" className="input" />
-						<ErrorMessage name="fechaNacimiento">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-					<div className="row">
-						Teléfono:
-						<Field name="telefono" type="text" className="input" />
-						<ErrorMessage name="telefono">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-
-
-					<div className="row">
-						Email:
-						<Field name="email" type="email" className="input" />
-						<ErrorMessage name="email">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-					<div className="row">
-						Password:
-						<Field name="password" type="password" className="input" />
-						<ErrorMessage name="password">
-							{message => <div className="error">{message}</div>}
-						</ErrorMessage>
-					</div>
-
-					<div className="row">
-						<button
-							type="submit"
-							className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
-							disabled={isSubmitting || !isValid} >
-							Submit
-						</button>
-					</div>
-				</Form>
-			</Card>
-
-			<Card title="Método de pago" className="tarjeta">
+  	render(){
+		return (
+			<div>	  
+			<Header/>
+			<div className="registro-container">
+			
+				<form onSubmit={this.handleSubmit}>
+					<Card title="Datos del usuario">
+							<div className="row">
+								Nombre: 
+								<input
+									type="text"
+									name="nombre"
+									value={this.state.nombre}
+									onChange={e => this.setState({ nombre: e.target.value })}
+									required
+								/>
+							</div>
+							<div className="row">
+								Apellidos: 
+								<input
+									type="text"
+									name="apellidos"
+									value={this.state.apellidos}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
+							<div className="row">
+								DNI: 
+								<input
+									type="text"
+									name="dni"
+									value={this.state.dni}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
+							<div className="row">
+								Fecha de nacimiento: 
+								<input
+									type="text"
+									name="fechaNacimiento"
+									value={this.state.fechaNacimiento}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
+							<div className="row">
+								Teléfono:
+								<input
+									type="text"
+									name="telefono"
+									value={this.state.telefono}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
 
 
-			</Card>
-		  	
-	  	</div>
-	</div>
- );
+							<div className="row">
+								Email:
+								<input
+									type="email"
+									name="email"
+									placeholder="Email"
+									value={this.state.email}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
+							<div className="row">
+								Password:
+								<input
+									type="password"
+									name="password"
+									placeholder="Password"
+									value={this.state.password}
+									onChange={this.handleChange}
+									required
+								/>
+							</div>
+
+							
+						</Card>
+
+						<Card title="Método de pago" className="tarjeta">
+
+
+						</Card>
+
+						<div className="row">
+							<button>
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		)
+ 	}
 }
-export default withFormik({
-	
-	 validate(values) {
-	        const errors = {};
-
-	        if (!values.password) {
-	            errors.password = 'Password is required';
-	        } else if (values.password.length < 8) {
-	            errors.password = 'Password must be at least 8 characters';
-	        }
-
-	        if (Object.keys(errors).length) {
-	            throw errors;
-	        }
-	    },
-
-	    handleSubmit(values, formikBag) {
-	        formikBag.setSubmitting(false);
-	        console.log(values);
-	    },
-	
-})(RegistroUsuario);
