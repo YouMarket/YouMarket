54ed96aee752681724ba32971fca50dc3ac4c203
Fran <fr4231gm@gmail.com>
2020-03-27 20:06:45 +0100

    Boton de paypal creado

--- src/main/you-market-front/src/Carro/index.js
+++ src/main/you-market-front/src/Carro/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PayPalButton } from "react-paypal-button-v2";
 import './styles.css';
 import Header from '../Header';
 import ProductoListado from '../ProductoListado'
@@ -20,6 +21,21 @@ function Carro() {
 				<div className="buttons">
 					<button className="save-cesta">Guardar como cesta</button>
 					<button className="button-finish">Terminar pedido</button>
+					 <PayPalButton
+				        amount="0.01"
+				        // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+				        onSuccess={(details, data) => {
+				          alert("Transaction completed by " + details.payer.name.given_name);
+				 
+				          // OPTIONAL: Call your server to save the transaction
+				          return fetch("/paypal-transaction-complete", {
+				            method: "post",
+				            body: JSON.stringify({
+				              orderID: data.orderID
+				            })
+				          });
+				        }}
+				      />
 				</div>
 			</div>
 		</div>
