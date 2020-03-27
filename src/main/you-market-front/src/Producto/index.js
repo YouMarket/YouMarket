3ca5a4ea0e5eba6216cdb473009571dff46034c6
manuelagt <manuelagt@icloud.com>
2020-03-27 18:31:51 +0100

    S2 - Producto - Hooks
    uso de useState en lugar de una variable dentro de la interfaz de producto

--- src/main/you-market-front/src/Producto/index.js
+++ src/main/you-market-front/src/Producto/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import plus from '../assets/plus.svg'
 import less from '../assets/less.svg'
@@ -10,23 +10,24 @@ interface Props {
 	supermercado: string,
 	precio: number,
 	unidad: string,
-	cantidad: number
 }
 
-function Producto({id, urlImagen, nombre, supermercado, precio, unidad, cantidad = 0}: Props) {
+function Producto({id, urlImagen, nombre, supermercado, precio, unidad}: Props) {
+	
+	const [cantidad, setCantidad] = useState(0);
 	
 	var idContador = "contador-"+id
 	var idMenos = "menos-"+id
 
 	function lessProduct(){
 		if(cantidad > 0){
-			cantidad = cantidad - 1
+			setCantidad(cantidad - 1)
 			document.getElementById(idContador).textContent = cantidad
 		}
 	}
 	
 	function plusProduct() {
-		cantidad = cantidad + 1
+		setCantidad(cantidad + 1)
 		document.getElementById(idContador).textContent = cantidad
 	}
 
@@ -37,12 +38,12 @@ function Producto({id, urlImagen, nombre, supermercado, precio, unidad, cantidad
   		<p className="producto-precio">{precio} {unidad}</p>
   		<p className="producto-nombre">{nombre}</p>
   		<p className="producto-supermercado">{supermercado}</p>
-		<div className="producto-editar-cantidad">
-			<img id={idMenos} className="menos" src={less} onClick={lessProduct}/>
-			<p id={idContador} className="contador">{cantidad}</p>
-			<img className="mas" src={plus} onClick={plusProduct}/>
-		</div>
   	</div>
+  	<div className="producto-editar-cantidad">
+  		<img id={idMenos} className="menos" src={less} onClick={lessProduct}/>
+  		<p id={idContador} className="contador">{cantidad}</p>
+  		<img className="mas" src={plus} onClick={plusProduct}/>
+	</div>
   </div>
  );
 }
