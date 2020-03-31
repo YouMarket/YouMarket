4920a9ba4cf33326151ba795d2648ca8d3ebc056
TCL90 <acmedespliegue2@gmail.com>
2020-03-31 13:26:03 +0200

    S2-Cesta/carrito- Guardar carrito como cesta front

--- src/main/you-market-front/src/Carro/index.js
+++ src/main/you-market-front/src/Carro/index.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import './styles.css';
 import Header from '../Header';
 import ProductoListado from '../ProductoListado';
+import { Formik,Field, Form } from 'formik';
 
 const precioFinal = 0.00
 function updatePrecioFinal(cantidad, precio){
@@ -12,6 +13,8 @@ function updatePrecioFinal(cantidad, precio){
 function Carro() {
 precioFinal = 0.00
 const[carrito, setCarrito] = useState([]);
+const[cestas, setCestas] = useState([]);
+	
 	const fetchCarrito = useCallback(() => {
 		return fetch('carrito')
 			.then(res => res.json())
@@ -25,6 +28,25 @@ const[carrito, setCarrito] = useState([]);
 	useEffect(() => {
 		fetchCarrito(carrito);
 	},[]);
+	
+	const fetchCestas = useCallback(() => {
+	    return fetch('cesta/user' , {headers: {
+		'Content-Type' : 'application/json',
+		'Accept' : 'application/json',
+		'Authorization' : 'Bearer ' + localStorage.getItem('auth')},
+		method:'GET'})
+	      .then(res => res.json())
+	      .then(cestas => {
+	        setCestas(cestas)
+	        console.log(cestas);
+	      });
+	  }, []);
+    console.log(cestas);
+
+
+	useEffect(() => {
+	    fetchCestas(cestas);
+	  }, []);
 	
   return(
 	<div>
@@ -49,7 +71,71 @@ const[carrito, setCarrito] = useState([]);
 				
 				<div className="price"><b>Precio final: </b>{Math.round(precioFinal * 100) / 100} €</div>
 				<div className="buttons">
-					{/* <button className="save-cesta">Guardar como cesta</button> */}
+
+				<Formik
+		         initialValues={{id: ''}}
+				 validate={values => {
+			        const errors = {};
+			        if (values.id=="") {
+			          errors.id = 'No puede estar vacío';
+			        }
+			        return errors;
+			      }}
+
+		         onSubmit={(values, { setSubmitting }) => {
+		           setTimeout(() => {
+		           	fetch(`/carritoACesta`, {
+		           			headers: {
+		           				"Content-Type": "application/json"
+		           			},
+		           			method:'POST',
+		           			body:JSON.stringify(values, null, 2)
+		           	}).then((response)=> {
+		           		setSubmitting=false;
+		           		alert(JSON.stringify(values, null, 2));
+
+		           	}).then(() =>
+		           	{history.push(`/cesta/${values.id}`);}
+		               )
+
+
+		           }, 400);
+		         }}
+		       >
+		         {({
+		           values,
+		           errors,
+		           touched,
+		           handleChange,
+		           handleBlur,
+		           handleSubmit,
+		           isSubmitting,
+		           /* and other goodies */
+		         }) => (
+		           <Form onSubmit={handleSubmit}>
+		           <div className="">
+		           
+		           <select name="id" id="id" onChange={handleChange}
+		           onBlur={handleBlur} value={values.id}>
+		           <option value=""></option>
+		           { cestas && cestas.map((cesta) => (
+		        		   
+		        		   			<option value={cesta.id}>{cesta.nombre}</option>
+		        		   			))}
+		           </select>
+		           {errors.id && touched.id && errors.id}
+
+		             <div className="grid2-carrito-cesta">
+		             <button type="submit" disabled={isSubmitting} className="">
+		             Añadir carrito a la cesta
+		             </button>
+		             </div>
+		            </div>
+		          </Form>
+
+		         )}
+		       </Formik>
+				
 					<a href="/pedido/create">
 					<button className="button-finish">Terminar pedido</button>
 					</a>
