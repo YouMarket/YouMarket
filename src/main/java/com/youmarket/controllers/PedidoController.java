55227379f69ccb0cd79ca623e34391b5624c670c
TCL90 <acmedespliegue2@gmail.com>
2020-03-31 20:04:35 +0200

    S2 - Pedido- Fix usuario

--- src/main/java/com/youmarket/controllers/PedidoController.java
+++ src/main/java/com/youmarket/controllers/PedidoController.java
@@ -7,10 +7,13 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import com.youmarket.configuration.security.CurrentUser;
+import com.youmarket.configuration.security.UserPrincipal;
 import com.youmarket.domain.CestaProducto;
 import com.youmarket.domain.Pedido;
 import com.youmarket.domain.Producto;
@@ -47,7 +50,7 @@ public class PedidoController {
     }
 	
 	@PostMapping("/create")
-    public ResponseEntity<Pedido> create(@RequestBody Pedido p, HttpSession session) throws URISyntaxException {
+    public ResponseEntity<Pedido> create(@RequestBody Pedido p, HttpSession session, @CurrentUser UserPrincipal currentUser) throws URISyntaxException {
 		
 		Date fechaHoraEntrega = new Date();
 		Date fechaHoraEnvio = new Date();
@@ -58,10 +61,13 @@ public class PedidoController {
 		p.setNombre("Pedido num. " + p.getId());
 		p.setOrdenEntrega(1);
 		p.setRetraso("No hubo retraso");
-
-		//TODO: CAMBIAR POR USUARIO LOGEADO
-		Usuario u = this.usuarioService.listaUsuarios().get(0);
-		p.setUsuario(u);
+		Optional<Usuario> user=this.usuarioService.findById(currentUser.getId());
+		
+		Usuario user2=null;
+		if(user.isPresent()) {
+			user2=user.get();
+		}
+		p.setUsuario(user2);
 		
 		//TODO: COSTE DEL PEDIDO
 		
@@ -76,7 +82,7 @@ public class PedidoController {
 			cp.setCantidad(carrito.get(prod));
 			cp.setCesta(pedidoGuardado);
 			cp.setId(prod, pedidoGuardado);
-			System.out.println(cp.getCesta().getId());
+
 			this.cpService.save(cp);
 		}
 		
