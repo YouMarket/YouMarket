88905e1449f987e3dc71e588e82e9efd8f41cf33
apcm <pi.com.xd@gmail.com>
2020-04-09 18:15:56 +0200

    S3 - Pedidos - Backend

--- src/main/java/com/youmarket/controllers/PedidoController.java
+++ src/main/java/com/youmarket/controllers/PedidoController.java
@@ -3,6 +3,7 @@ package com.youmarket.controllers;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -11,15 +12,6 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.youmarket.configuration.security.CurrentUser;
 import com.youmarket.configuration.security.UserPrincipal;
@@ -30,7 +22,17 @@ import com.youmarket.domain.Usuario;
 import com.youmarket.domain.form.FormPedidos;
 import com.youmarket.services.CestaProductoService;
 import com.youmarket.services.PedidoService;
+import com.youmarket.services.SuscripcionService;
 import com.youmarket.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("pedido")
@@ -44,6 +46,9 @@ public class PedidoController {
 
 	@Autowired
 	private CestaProductoService cpService;
+
+	@Autowired
+	private SuscripcionService susService;
 	
 	@GetMapping("/{id}")
     public ResponseEntity<Object> pedidoPorId(@Valid @PathVariable Integer id) {
@@ -205,6 +210,151 @@ public class PedidoController {
 		
 		return ResponseEntity.created(new URI ("/pedido/)" + pedidoGuardado.getId())).body(pedidoGuardado);
 		
-    }
+	}
+	
+	@PostMapping("/create3")
+	public ResponseEntity<List<Pedido>> createPedidos(@RequestBody FormPedidos pedidos, HttpSession session, @CurrentUser UserPrincipal currentUser) throws URISyntaxException {
+		
+		Usuario user = this.usuarioService.findById(currentUser.getId()).orElse(null);
+
+		Date now = new Date();
+		
+		@SuppressWarnings("unchecked")
+		Map<Producto, Integer> carrito = (Map<Producto, Integer>)session.getAttribute("SESSION_CARRITO");
+
+		Pedido p1s = new Pedido();
+		Pedido p2s = new Pedido();
+		Pedido p3s = new Pedido();
+		Pedido p4s = new Pedido();
+		
+		if(pedidos.getDireccion4() != null){
+			user.getSuscripcion().setEnvios(user.getSuscripcion().getEnvios() - 4);
+		} else if(pedidos.getDireccion3() != null){
+			user.getSuscripcion().setEnvios(user.getSuscripcion().getEnvios() - 3);
+		} else if(pedidos.getDireccion2() != null){
+			user.getSuscripcion().setEnvios(user.getSuscripcion().getEnvios() - 2);
+		} else if(pedidos.getDireccion1() != null){
+			user.getSuscripcion().setEnvios(user.getSuscripcion().getEnvios() - 1);
+		}
+		this.susService.save(user.getSuscripcion());
+		if(pedidos.getDireccion1() != null){
+			Pedido p1 = new Pedido();
+			p1.setCpostal(pedidos.getCpostal1());
+			p1.setDireccion(pedidos.getDireccion1());
+			p1.setFechaEnvio(pedidos.getFechaEnvio1());
+			p1.setHoraEnvioFin(pedidos.getHoraEnvioFin1());
+			p1.setHoraEnvioIni(pedidos.getHoraEnvioIni1());
+			p1.setPoblacion(pedidos.getPoblacion1());
+			p1.setProvincia(pedidos.getProvincia1());
+			
+			p1.setFechaHoraPedido(now);
+			p1.setNombre("Pedido num. " + p1.getId());
+			p1.setOrdenEntrega(1);
+			p1.setRetraso("No hubo retraso");
+			p1.setUsuario(user);
+			if (pedidos.getCestaId1() == 0){
+				p1s = this.meterCarrito(carrito, p1);
+			} else {
+				p1s = this.meterCesta(pedidos.getCestaId1(), p1);
+			}
+		}
+
+		if(pedidos.getDireccion2() != null){
+			Pedido p2 = new Pedido();
+			p2.setCpostal(pedidos.getCpostal2());
+			p2.setDireccion(pedidos.getDireccion2());
+			p2.setFechaEnvio(pedidos.getFechaEnvio2());
+			p2.setHoraEnvioFin(pedidos.getHoraEnvioFin2());
+			p2.setHoraEnvioIni(pedidos.getHoraEnvioIni2());
+			p2.setPoblacion(pedidos.getPoblacion2());
+			p2.setProvincia(pedidos.getProvincia2());
+			
+			p2.setFechaHoraPedido(now);
+			p2.setNombre("Pedido num. " + p2.getId());
+			p2.setOrdenEntrega(2);
+			p2.setRetraso("No hubo retraso");
+			p2.setUsuario(user);
+			if (pedidos.getCestaId2() == 0){
+				p2s = this.meterCarrito(carrito, p2);
+			} else {
+				p2s = this.meterCesta(pedidos.getCestaId2(), p2);
+			}
+		}
+
+		if(pedidos.getDireccion3() != null){
+			Pedido p3 = new Pedido();
+			p3.setCpostal(pedidos.getCpostal3());
+			p3.setDireccion(pedidos.getDireccion3());
+			p3.setFechaEnvio(pedidos.getFechaEnvio3());
+			p3.setHoraEnvioFin(pedidos.getHoraEnvioFin3());
+			p3.setHoraEnvioIni(pedidos.getHoraEnvioIni3());
+			p3.setPoblacion(pedidos.getPoblacion3());
+			p3.setProvincia(pedidos.getProvincia3());
+			
+			p3.setFechaHoraPedido(now);
+			p3.setNombre("Pedido num. " + p3.getId());
+			p3.setOrdenEntrega(3);
+			p3.setRetraso("No hubo retraso");
+			p3.setUsuario(user);
+			if (pedidos.getCestaId3() == 0){
+				p3s = this.meterCarrito(carrito, p3);
+			} else {
+				p3s = this.meterCesta(pedidos.getCestaId3(), p3);
+			}
+		}
+
+		if(pedidos.getDireccion4() != null){
+			Pedido p4 = new Pedido();
+			p4.setCpostal(pedidos.getCpostal4());
+			p4.setDireccion(pedidos.getDireccion4());
+			p4.setFechaEnvio(pedidos.getFechaEnvio4());
+			p4.setHoraEnvioFin(pedidos.getHoraEnvioFin4());
+			p4.setHoraEnvioIni(pedidos.getHoraEnvioIni4());
+			p4.setPoblacion(pedidos.getPoblacion4());
+			p4.setProvincia(pedidos.getProvincia4());
+			
+			p4.setFechaHoraPedido(now);
+			p4.setNombre("Pedido num. " + p4.getId());
+			p4.setOrdenEntrega(4);
+			p4.setRetraso("No hubo retraso");
+			p4.setUsuario(user);
+			if (pedidos.getCestaId4() == 0){
+				p4s = this.meterCarrito(carrito, p4);
+			} else {
+				p4s = this.meterCesta(pedidos.getCestaId4(), p4);
+			}
+		}
+		List<Pedido> res = Arrays.asList(p1s,p2s,p3s,p4s);
+		return ResponseEntity.ok(res);
+
+	}
+
+	public Pedido meterCarrito(Map<Producto, Integer> carrito, Pedido p){
+		Pedido guardado = this.pedidoService.save(p);
+		List<Producto> keys = new ArrayList<>(carrito.keySet());
+		for(Producto prod : keys){
+			CestaProducto cp = new CestaProducto();
+			cp.setProducto(prod);
+			cp.setCantidad(carrito.get(prod));
+			cp.setCesta(guardado);
+			cp.setId(prod, guardado);
+			this.cpService.save(cp);
+		}
+		return guardado;
+	}
+
+	public Pedido meterCesta(int idCesta, Pedido p){
+		Pedido guardado = this.pedidoService.save(p);
+		List<CestaProducto> cpCesta = this.cpService.cpPorCesta(idCesta);
+		for(CestaProducto cp : cpCesta){
+			CestaProducto pedidoProducto = new CestaProducto();
+			cp.setProducto(cp.getProducto());
+			cp.setCantidad(cp.getCantidad());
+			cp.setCesta(guardado);
+			cp.setId(cp.getProducto(), guardado);
+			this.cpService.save(pedidoProducto);
+		}
+		return guardado;
+	}
 
 }
