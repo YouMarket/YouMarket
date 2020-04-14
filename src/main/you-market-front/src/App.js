fc67417630b24c70ed1fce5c39a6cae2b317170b
AlvaroEsteban <alvaroestebancastrillo@gmail.com>
2020-04-14 21:20:50 +0200

    Correcciones merge

--- src/main/you-market-front/src/App.js
+++ src/main/you-market-front/src/App.js
@@ -31,8 +31,6 @@ import ShowCesta from './ShowCesta';
 import EditCesta from './EditCesta';
 import Error from './Misc';
 import CestaProductos from './CestaProductos';
-import TerminosYCondiciones from './TerminosCondiciones';
-import AlertaBrecha from './AlertaBrecha';
 import Facturas from './Perfil/Facturas';
 import SubscripcionForm from './Perfil/DatosUsuario/suscripcionForm'
 import TerminosYCondiciones from './TerminosCondiciones';
