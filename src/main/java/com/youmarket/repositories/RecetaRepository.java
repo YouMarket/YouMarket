b7ed85643de6e1cf0218eeee181e160b0c6919da
apcm <pi.com.xd@gmail.com>
2020-04-19 17:36:11 +0200

    S3 - Recetas - Backend

---
+++ src/main/java/com/youmarket/repositories/RecetaRepository.java
@@ -0,0 +1,14 @@
+package com.youmarket.repositories;
+
+import java.util.List;
+
+import com.youmarket.domain.Receta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface RecetaRepository extends JpaRepository<Receta, Integer>{
+
+    @Query("select d.recetas from Dieta d where d.id = ?1")
+    List<Receta> recetasDeDietaId(int dietaId);
+}
---
+++ src/main/java/com/youmarket/services/RecetaService.java
@@ -0,0 +1,38 @@
+package com.youmarket.services;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.youmarket.domain.Receta;
+import com.youmarket.repositories.RecetaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecetaService {
+
+    @Autowired
+    private RecetaRepository recetaRepository;
+
+    public List<Receta> findAll(){
+        return this.recetaRepository.findAll();
+    }
+
+    public Receta findById(int id){
+        return this.recetaRepository.findById(id).orElse(null);
+    }
+
+    public List<Receta> findByDietaId(int dietaId) {
+        return this.recetaRepository.recetasDeDietaId(dietaId);
+    }
+
+    public Receta save(@Valid Receta r){
+        return this.recetaRepository.save(r);
+    }
+
+    public void delete (Receta r){
+        this.recetaRepository.delete(r);
+    }
+}
