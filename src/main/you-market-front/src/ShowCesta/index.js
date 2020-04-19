9178b38f8ded20084e3482fcfcca3437454434e1
fr4231gm <fr4231gm@gmail.com>
2020-04-19 21:51:23 +0200

    Arreglo mergin

--- src/main/you-market-front/src/ShowCesta/index.js
+++ src/main/you-market-front/src/ShowCesta/index.js
@@ -98,92 +98,33 @@ function ShowCesta() {
 		history.push('/login');
 	}
 
+	return (
+		<div>
+			<Header />
 
+			<div className="cesta-container-show">
 
-  return(
-<div>
-  <Header/>
+				<Cesta nombre={cesta.nombre} id={cesta.id} total="" />
+				<h2 className="show-cesta-h">Productos</h2>
+				<div className="separador"></div>
+				{productoCesta && productoCesta.map((productoC) => (
 
-	  <div className="cesta-container-show">
+					<div key={productoC.producto.id} className="div-productos-cesta">
+						<Link to={`/show/producto/${productoC.producto.id}`}> {productoC.producto.nombre}
+						</Link> x{productoC.cantidad}
+						<img src={productoC.url} />
+					</div>
 
-	  <Cesta nombre={cesta.nombre} id={cesta.id} total=""/>
-	<h2 className="show-cesta-h">Productos</h2>
-	  <div className="separador"></div>
-	  { productoCesta && productoCesta.map((productoC) => (
+				))}
+				<p className="cesta-total">Total: {total}€</p>
 
-			    <div key={productoC.producto.id} className="div-productos-cesta">
-			    
-			    <Link to={`/show/producto/${productoC.producto.id}`}> {productoC.producto.nombre}
-			    </Link> x{productoC.cantidad}
-			    <img src={productoC.url} alt={productoC.nombre}/>
-
-
-			    
-			    </div>
-
-	           ))}
-	  <p className="cesta-total">Total: {total}€</p>
-	    
-	    <Formik
-         initialValues={{id}}
-
-         onSubmit={(values, { setSubmitting }) => {
-           setTimeout(() => {
-           	fetch(`/cestaACarrito`, {headers: {
-        		'Content-Type' : 'application/json',
-        		'Accept' : 'application/json',
-        		'Authorization' : 'Bearer ' + localStorage.getItem('auth')},
-           			method:'POST',
-           			body:JSON.stringify(values, null, 2)
-           	}).then((response)=> {
-           		setSubmitting=false;
-
-           	}).then(() =>
-           	{history.push("/carro");}
-               )
-
-
-           }, 400);
-         }}
-       >
-         {({
-           values,
-           errors,
-           touched,
-           handleChange,
-           handleBlur,
-           handleSubmit,
-           isSubmitting,
-           /* and other goodies */
-         }) => (
-           <form onSubmit={handleSubmit}>
-           <div className="grid-form-cesta">
-           
-             <input
-             id="id"
-               type="hidden"
-               name="id"
-               onChange={handleChange}
-               onBlur={handleBlur}
-               value={id}
-             	className="id-input-cesta"
-             />
-
-             <div className="grid2-carrito-cesta">
-             <button type="submit" disabled={isSubmitting} className="submit-cesta-carrito">
-             Añadir
-             </button>
-             </div>
-             </div>
-           </form>
-
-         )}
-       </Formik>
-
-	           
-	  </div>
-
-  </div>
- );
+				<div className="grid2-carrito-cesta">
+					<button onClick={() => {storeAsCarrito()}}  className="submit-cesta-carrito">
+						Añadir
+            		</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default ShowCesta;
