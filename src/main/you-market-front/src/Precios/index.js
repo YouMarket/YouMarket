bbebc7d27d443d511e4eb2faf688e71b5e3b3434
Lucía del Carmen Fuentes <luciadelcarmenfuentes@gmail.com>
2020-04-20 21:16:57 +0200

    Página de precios

--- src/main/you-market-front/src/Precios/index.js
+++ src/main/you-market-front/src/Precios/index.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import './styles.css';
 import Header from '../Header';
-import cesta from '../assets/basket.svg'
-import suscripcion from '../assets/subscription.svg'
-import furgoneta from '../assets/van.svg'
-import dieta from '../assets/fruit.svg'
+import cart from '../assets/supermarket.svg'
+import nutritionist from '../assets/nutritionist.svg'
 
 function Precios() {
 
   return(
       <div>
         <Header/>
-        <div className="preciospage">
-            <p className="preciospage-welcome">Suscripciones</p>
-            <div className="preciospage-services container">
-                <div className="preciospage-service service-left">
-                    <img className="service-image" src="https://image.flaticon.com/icons/svg/1170/1170678.svg" alt="estandar" />
-                    <div className="service-information">
-                        <p className="service-envios">Servicio est·ndar</p>
-                        <p className="service-euros"></p>
-                        <p className="service-text">Podr·s realizar una suscripciÛn que incluir· un n˙mero de envÌos al mes.
-                         				TambiÈn podr·s preparar cestas para que luego solo tengas que seleccionarlas.
+        <div className="preciospage-container container">
+            <p className="preciospage-title">Suscripciones</p>
+            <div className="preciospage-suscripciones container">
+                <div className="preciospage-card card-left">
+                    <img className="card-image" src={cart} alt="estandar" />
+                    <div className="card-information">
+                        <p className="card-envios">Servicio est√°ndar</p>
+                        <p className="card-euros"></p>
+                        <p className="card-text">Podr√°s realizar una suscripci√≥n que incluir√° un n√∫mero de env√≠os al mes.
+                         				Tambi√©n podr√°s preparar cestas para que luego solo tengas que seleccionarlas.
                         </p>
                     </div>
                 </div>
-                <div className="preciospage-service service-right">
-                    <img className="service-image" src="https://image.flaticon.com/icons/svg/1597/1597203.svg" alt="dietista"/>
-                    <div className="service-information">
-                        <p className="service-envios">Servicio de dietistas</p>
-                        <p className="service-euros"></p>
-                        <p className="service-text">
-                        		El servicio de dietas te dar· acceso a una serie de dietas que preparan nuestro equipo de dietistas.
-                        		Incluye productos de temporada y garantiza una dieta equilibrada. Adem·s, ofrecemos una serie de
+                <div className="preciospage-card card-right">
+                    <img className="card-image" src={nutritionist} alt="dietista"/>
+                    <div className="card-information">
+                        <p className="card-envios">Servicio de dietistas</p>
+                        <p className="card-euros"></p>
+                        <p className="card-text">
+                        		El servicio de dietas te dar√° acceso a una serie de dietas que preparan nuestro equipo de dietistas.
+                        		Incluye productos de temporada y garantiza una dieta equilibrada. Adem√°s, ofrecemos una serie de
                         		 recetas para cada dieta.
                         </p>
                     </div>
@@ -40,28 +38,25 @@ function Precios() {
   		</div>    
         
         
-        <div className="preciospage2">
-            <p className="preciospage-welcome">Precios</p>
-            <div className="preciospage-services container">
-                <div className="preciospage-service service-left">
-                    <div className="service-information">
-                        <p className="service-envios">Servicio est·ndar</p>
-                        <p className="service-text">
-                        	<ul>
-                        		<li> 1 envÌo: 6 Ä </li>
-                        		<li> 2 envÌos: 10 Ä </li>
-                        		<li> 3 envÌos: 14 Ä </li>
-                        		<li> 4 envÌos: 17 Ä </li>
-                        		
-                        	</ul>
-                        </p>
+        <div className="preciospage-container container">
+            <p className="preciospage-title">Precios</p>
+            <div className="preciospage-precios container">
+                <div className="preciospage-card card-left">
+                    <div className="card-information">
+                        <p className="card-envios">Servicio est√°ndar</p>
+                        <ul className="card-text">
+                            <li> 1 env√≠o: 6 ‚Ç¨ </li>
+                            <li> 2 env√≠os: 10 ‚Ç¨ </li>
+                            <li> 3 env√≠os: 14 ‚Ç¨ </li>
+                            <li> 4 env√≠os: 17 ‚Ç¨ </li>
+                        </ul>
                     </div>
                 </div>
-                <div className="preciospage-service service-right">
-                    <div className="service-information">
-                        <p className="service-envios">Servicio de dietistas</p>
-                        <p className="service-euros">
-                         + 3 Ä
+                <div className="preciospage-card card-right">
+                    <div className="card-information">
+                        <p className="card-envios">Servicio de dietistas</p>
+                        <p className="card-euros">
+                         + 3 ‚Ç¨
                         </p>
 
                     </div>
