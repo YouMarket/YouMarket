df1e92efc77e63ff72ae1b5fff6477ef8244ee79
apcm <pi.com.xd@gmail.com>
2020-04-20 18:41:05 +0200

    Comprobaciones de suscripción con dietista

--- src/main/java/com/youmarket/controllers/DietaController.java
+++ src/main/java/com/youmarket/controllers/DietaController.java
@@ -1,24 +1,24 @@
 package com.youmarket.controllers;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import com.youmarket.configuration.security.CurrentUser;
+import com.youmarket.configuration.security.UserPrincipal;
+import com.youmarket.domain.Dieta;
+import com.youmarket.domain.Usuario;
+import com.youmarket.services.DietaService;
+import com.youmarket.services.UsuarioService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.youmarket.domain.Dieta;
-import com.youmarket.services.DietaService;
 
 @RestController
 @RequestMapping("dieta")
@@ -26,16 +26,23 @@ public class DietaController {
 
 	@Autowired
 	private DietaService dietaService;
+
+	@Autowired 
+	private UsuarioService usuarioService;
 		
 	@RequestMapping("/list")
-	public List<Dieta> listDietas(Model model){
+	public List<Dieta> listDietas(Model model, @CurrentUser UserPrincipal logged){
+		Usuario user = usuarioService.findById(logged.getId()).orElse(null);
+		Assert.isTrue(user.getSuscripcion().isDietista());
 		List<Dieta> dietas=dietaService.findAll();
 			
 		return dietas;
 	}
 	
 	@GetMapping("/{id}")
-    public ResponseEntity<Object> dietaPorId(@Valid @PathVariable Integer id) {
+    public ResponseEntity<Object> dietaPorId(@Valid @PathVariable Integer id, @CurrentUser UserPrincipal logged) {
+		Usuario user = usuarioService.findById(logged.getId()).orElse(null);
+		Assert.isTrue(user.getSuscripcion().isDietista());
         return ResponseEntity.ok(dietaService.findById(id));
     }
 
