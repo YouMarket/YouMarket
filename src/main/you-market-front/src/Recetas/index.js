12e3fb84c5548a4a226d7896e6022b7b417a8c32
Lucía del Carmen Fuentes <luciadelcarmenfuentes@gmail.com>
2020-04-20 17:51:22 +0200

    Recetas terminado

--- src/main/you-market-front/src/Recetas/index.js
+++ src/main/you-market-front/src/Recetas/index.js
@@ -29,14 +29,14 @@ function Recetas() {
 		history.push('/login');
 	}
     
-	if(localStorage.getItem('dietasCheck')===0){
+	if(localStorage.getItem('dietasCheck')==0){
 		history.push('/404');
 	}
 
   return(
 	<div>	  
 	  <Header/>
-	  {localStorage.getItem('dietasCheck')===1 ?
+	  {localStorage.getItem('dietasCheck')==1 ?
 	  		(<div>
 			<h2 className="container">¡Aquí tienes las recetas de la dieta que has seleccionado!</h2>
 			<div className="recetas-container container">
