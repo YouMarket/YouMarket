5fbaace8aee3ef3b84bfc5a9d9679d162c221aea
Antonio <39269864+apcm@users.noreply.github.com>
2020-04-20 18:47:09 +0200

    Validación en back. (#94)
    * S3 - Validación back
    
    * Update Usuario.java
    
    * Update Pedido.java
    
    * Comprobaciones de suscripción con dietista

--- src/main/java/com/youmarket/domain/Supermercado.java
+++ src/main/java/com/youmarket/domain/Supermercado.java
@@ -5,6 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.SafeHtml;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,15 +24,24 @@ public class Supermercado {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
+	@NotBlank
+	@SafeHtml
 	@Column(name="nombre", length=50)
 	private String nombre;
 	
+	@NotBlank
+	@SafeHtml
 	@Column(name="descripcion", length = 250)
 	private String descripcion;
 	
+	@NotBlank
+	@SafeHtml
+	@Pattern(regexp ="\\b\\d{5}\\b")
 	@Column(name="c_postal", length = 5)
 	private String cPostal;
 	
+	@NotBlank
+	@SafeHtml
 	@Column(name="intolerancia", length = 250)
 	private String intolerancia;
 
