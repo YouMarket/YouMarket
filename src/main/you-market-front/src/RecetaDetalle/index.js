7a04dbd3042da8e36ec4360b12da543092ca3753
fr4231gm <fr4231gm@gmail.com>
2020-04-21 20:27:36 +0200

    Arreglo detalle receta

--- src/main/you-market-front/src/RecetaDetalle/index.js
+++ src/main/you-market-front/src/RecetaDetalle/index.js
@@ -16,7 +16,7 @@ function RecetaDetalle() {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
 				'Authorization' : 'Bearer ' + localStorage.getItem('auth')},
-			method:'POST'
+			method:'GET'
 		})
 	      .then(res => res.json())
 	      .then(receta => {
